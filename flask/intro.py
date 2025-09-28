0fe7ed1d88f416169d6bc4cb148484477beca594
avishka-m <avishka.prog@gmail.com>
2025-09-28 10:32:01 +0530

    render template is tested

--- flask/intro.py
+++ flask/intro.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Welcome to the fsk "
+    return render_template('index.html')
+
+@app.route('/my')
+def my():
+    return "Hello this is my"
+
+
 
 
 
