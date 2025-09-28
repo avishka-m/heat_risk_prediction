f41008aec1a88ab9c8d1bd29ef3e8de10abc99de
avishka-m <avishka.prog@gmail.com>
2025-09-28 21:18:33 +0530

    last commit in flask lerning

--- flask/intro.py
+++ flask/intro.py
@@ -7,7 +7,6 @@ import cv2
 BASE_DIR = os.path.dirname(__file__)
 model_path = os.path.join(BASE_DIR, "svm_face_recognisation.sav")
 
-print("Loading model from:", model_path)
 model = joblib.load(model_path)
 
 category_dict={0:'Avishka',1:"Dulanga",2:"Iduwara",3:"Isuru"}
@@ -24,45 +23,45 @@ def getresults():
     print(result)
     return('results printed')
 
-# @app.route('/face')
-# def face():
-    # face_classifire = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+@app.route('/face')
+def face():
+    face_classifire = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-    # # test_img_names = os.listdir(test_data_path)
+    # test_img_names = os.listdir(test_data_path)
 
-    # source = cv2.VideoCapture(0)
+    source = cv2.VideoCapture(0)
 
 
-    # while(True):
-    # # for test_img in test_img_names:
-    #     # img_path = os.path.join(test_data_path,test_img) 
-    #     # test_img = cv2.imread(img_path)
+    while(True):
+    # for test_img in test_img_names:
+        # img_path = os.path.join(test_data_path,test_img) 
+        # test_img = cv2.imread(img_path)
 
-    #     ret,img = source.read()
+        ret,img = source.read()
 
-    #     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-    #     faces = face_classifire.detectMultiScale(gray)
-    #     for (x,y,w,h) in faces:
-    #         cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        faces = face_classifire.detectMultiScale(gray)
+        for (x,y,w,h) in faces:
+            cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
 
-    #         croped_face = gray[y:y+h,x:x+w]
-    #         croped_face = cv2.resize(croped_face,(50,50))
-    #         croped_original = croped_face
-    #         croped_face = croped_face.reshape(1,50*50)
+            croped_face = gray[y:y+h,x:x+w]
+            croped_face = cv2.resize(croped_face,(50,50))
+            croped_original = croped_face
+            croped_face = croped_face.reshape(1,50*50)
 
-    #         result = model.predict(croped_face)
-    #         name = category_dict[result[0]]
+            result = model.predict(croped_face)
+            name = category_dict[result[0]]
 
-    #         cv2.putText(img,name,(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,1.5,(255,0,0),2)
-    #         cv2.imshow('GRAY',croped_original) 
-    #     cv2.imshow("IVE", img)
+            cv2.putText(img,name,(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,1.5,(255,0,0),2)
+            cv2.imshow('GRAY',croped_original) 
+        cv2.imshow("IVE", img)
 
-    #     k = cv2.waitKey(10)
-    #     if(k==27):
-    #         break
+        k = cv2.waitKey(10)
+        if(k==27):
+            break
 
-    # cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
 
 
 app.run(debug=True)
